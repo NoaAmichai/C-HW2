f97ea09e6379fc9624ce0f0de0e5d122ed88f354
Aviost111 <Aviostroff@yahoo.com>
2022-11-23 15:59:50 +0200

    .

--- my_mat.c
+++ my_mat.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #define N 10
 
-int[][] getMat(){
-    int [][] mat=new int[N][N];
+void getMat(int mat[N][N]){
     int num;
     for (int i=0;i<N-1;i++){
         for(int j=0;j<N-1;j++){
